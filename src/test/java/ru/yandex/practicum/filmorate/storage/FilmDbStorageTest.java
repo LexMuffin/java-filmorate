76651002daa3fa39822500ea3099fe3ad7b299d9
Mihail <kto-kuda-a-ti-suda@yandex.ru>
2025-08-01 20:34:41 +0300

    Initial commit v2 FH-12

--- src/test/java/ru/yandex/practicum/filmorate/storage/FilmDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/FilmDbStorageTest.java
@@ -16,7 +16,6 @@ import ru.yandex.practicum.filmorate.storage.mpa.MpaStorage;
 
 import java.time.LocalDate;
 import java.util.Collection;
-import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
